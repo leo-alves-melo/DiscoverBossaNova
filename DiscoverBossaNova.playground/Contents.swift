94ceb1a5568b76e3dc59e2ed3aad4b835cd46b49
Leonardo Alves de Melo <leonardo.alves.melo.1995@gmail.com>
2019-03-24 10:13:41 -0300

    #develop: updated readMe and markup.

--- DiscoverBossaNova.playground/Contents.swift
+++ DiscoverBossaNova.playground/Contents.swift
@@ -2,7 +2,7 @@
  
  # Discover Bossa Nova
  
- Have you ever heard about Bossa Nova? It's a music style created in Brazil in the 60's. Learn about it and enjoy a great music-culture experience in this Swift-based Playground!
+ Have you ever heard about Bossa Nova? It's a music style created in Brazil in the 60's. Learn about it, helps Tom Jobim to write the popular song "Garota de Ipanema" and enjoy a great music-culture experience in this Swift-based Playground!
  
  */
 import PlaygroundSupport
